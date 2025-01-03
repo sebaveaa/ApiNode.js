4bc5a58f1dd44934fe2ecbb5472acc07bdae478f
Sebastian Llambi <sebastianllambi@gmail.com>
2025-01-03 15:48:21 -0400

    correccion de error en rutas

--- src/routes/chisteRoutes.js
+++ src/routes/chisteRoutes.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
 
-Router.put('/:id',chisteController.updateChiste);
-Router.get('/random', chisteController.getRandomChisteId);
+
 const chisteController = require("../controllers/chisteController")
 const Router = express.Router();
-const Chiste = require("../models/chiste.model")
+
+Router.put('/:id',chisteController.updateChiste);
+Router.get('/random', chisteController.getRandomChisteId);
 Router.get("/:f",chisteController.getChiste)
 Router.post("/Propio", chisteController.postChiste);
 
