bf20620858d33123df7d6ca2be0c695c89a89fb3
Sebastian Llambi <sebastianllambi@gmail.com>
2025-01-03 20:44:17 -0400

    arreglo del merge

--- src/routes/chisteRoutes.js
+++ src/routes/chisteRoutes.js
@@ -139,16 +139,9 @@ Router.post("/Propio", chisteController.postChiste);
  * @returns {object} 404 - No se encontró el chiste
  * @returns {object} 400 - Error de validación u otro error
  */
-Router.put('/:id',chisteController.updateChiste);
+Router.put('/:id',chisteController.putChiste);
 /**
- * @route GET /api/chistes/fuente/random
- * @desc Obtiene el ID de un chiste aleatorio
- * @access Public
- * @returns {object} 200 - Un objeto con el ID del chiste aleatorio
- * @returns {object} 404 - No se encontraron chistes
- * @returns {object} 500 - Error del servidor
- */
-Router.get('/random', chisteController.getRandomChisteId);
+
 /** 
     * @route GET /api/chistes/fuente/Propio/get/id:
     * @desc Obtiene un chiste por su ID
